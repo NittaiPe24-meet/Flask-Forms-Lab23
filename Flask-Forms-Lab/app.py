c91b88fafbe262e913ae134d6eb3f39a4fb32710
NittaiPe24-meet <nittai.peri@gmail.com>
2023-07-19 18:10:36 +0300

    nittwo

--- Flask-Forms-Lab/app.py
+++ Flask-Forms-Lab/app.py
@@ -7,20 +7,89 @@ app = Flask(  # Create a flask app
 	static_folder='static'  # Name of directory for static files
 )
 
-
-username = "siwarha"
-password = "123"
+@app.route('/home')
+def home():
+	return render_template('home.html')
+                                                     
+username = "nittai"
+password = "1234"
 facebook_friends=["Loai","Kenda","Avigail", "George", "Fouad", "Gi"]
 
 
-@app.route('/')  # '/' for the default page
+@app.route('/', methods=["GET", "POST"])  # '/' for the default page
 def login():
-  return render_template('login.html')
-  
-
+  if request.method == "GET":
+  	return render_template('login.html')
+  else:
+  	html_name = request.form['username']
+  	html_password = request.form['password']
+  	if (username == html_name and password == html_password):
+  		return redirect(url_for('home'))
+  	else:
+  		return render_template('login.html')
 
 
 if __name__ == "__main__":  # Makes sure this is the main process
 	app.run( # Starts the site
     debug=True
 	)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#1 check gor username and password, and if the user logs in successfully - enter the home page
